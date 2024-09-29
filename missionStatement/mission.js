5014f1ad4d2dc273089c6ca85a3267225fe42b18
CarterJSmith <smi24043@byui.edu>
2024-09-28 21:24:52 -0600

    updated background color

--- missionStatement/mission.js
+++ missionStatement/mission.js
@@ -1,7 +1,10 @@
+// We set themeSwitch to the selector element.
 const themeSwitch = document.getElementById("theme");
 
+// listens for a change on the selector 
 themeSwitch.addEventListener("change", changeTheme);
 
+// this changes the theme 
 function changeTheme() {
     const body = document.body; 
     const logo = document.getElementById("logo-footer"); 
