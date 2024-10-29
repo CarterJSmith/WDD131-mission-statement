9619228fb8e91ad2e656496f0a7009fba25c2f0d
CarterJSmith <smi24043@byui.edu>
2024-10-29 14:34:44 -0600

    fixed blog

--- missionStatement/build-a-blog/blog.js
+++ missionStatement/build-a-blog/blog.js
@@ -23,20 +23,20 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}//,
-    // {
-    //     id: 3,
-    //     title: "Belgariad Book One: Pawn of Prophecy",
-    //     date: "Feb 12, 2022",
-    //     description:
-    //         "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
-    //     imgSrc:
-    //         "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
-    //     imgAlt: "Book cover for Pawn of Prophecy",
-    //     ages: "12-16",
-    //     genre: "Fantasy",
-    //     stars: "⭐⭐⭐⭐⭐"
-    // }
+	},
+    {
+        id: 3,
+        title: "Belgariad Book One: Pawn of Prophecy",
+        date: "Feb 12, 2022",
+        description:
+            "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his \"Aunt Pol\" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+        imgSrc:
+            "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+        imgAlt: "Book cover for Pawn of Prophecy",
+        ages: "12-16",
+        genre: "Fantasy",
+        stars: "⭐⭐⭐⭐⭐"
+    }
 ]
 
 
@@ -51,15 +51,6 @@ if (!mainContent) {
 
 // Function to generate articles
 function displayArticles() {
-    // filterElements = document.querySelectorAll('.div');
-    // filterElements.classList.add('filters');
-
-    // filterElements.innerHTML = `
-    //         <div class="filters">
-    //             <p>filters will go here</p>
-    //         </div>
-    //         `;
-    // mainContent.appendChild(filterElements);
 
     articles.forEach(article => {
         const articleElement = document.createElement('div');
@@ -85,10 +76,6 @@ function displayArticles() {
         // Append the new article to the main content
         mainContent.appendChild(articleElement);
     });
-
-    // Append filters at the end, on the next row
-    // const filters = document.querySelector('.filters');
-    // mainContent.appendChild(filters);
 }
 
 // Call the function to display articles
